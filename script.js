47b7bf6ec8a4999483e61f33c5bba0f78826ab24
steve hanlon <rshanlon@gmail.com>
2019-01-24 22:15:08 -0800

    Initial commit; copy code over from CodePen; add audio SFX

---
+++ script.js
@@ -0,0 +1,17 @@
+// Instantiate audio
+const tone1 = new Audio("audio/tone1.mp3");
+const tone2 = new Audio("audio/tone2.mp3");
+const tone3 = new Audio("audio/tone3.mp3");
+const tone4 = new Audio("audio/tone4.mp3");
+
+
+// Make Objects
+const pad1 = document.querySelector('#pad1');
+const pad2 = document.querySelector('#pad2');
+const pad3 = document.querySelector('#pad3');
+const pad4 = document.querySelector('#pad4');
+
+pad1.addEventListener('mousedown', () => tone1.play());
+pad2.addEventListener('mousedown', () => tone2.play());
+pad3.addEventListener('mousedown', () => tone3.play());
+pad4.addEventListener('mousedown', () => tone4.play());
