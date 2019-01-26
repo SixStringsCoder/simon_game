df432e855b7ddf43ef95adde82a210d3983a77c9
steve hanlon <rshanlon@gmail.com>
2019-01-25 23:26:01 -0800

    Added control panel sliders and buttons along with CSS

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const tone2 = new Audio("audio/tone2.mp3");
 const tone3 = new Audio("audio/tone3.mp3");
 const tone4 = new Audio("audio/tone4.mp3");
 
-
-// Make Objects
+// Set Object References
 const pad1 = document.querySelector('#pad1');
 const pad2 = document.querySelector('#pad2');
 const pad3 = document.querySelector('#pad3');
 const pad4 = document.querySelector('#pad4');
 
+// Listeners and Handlers
 pad1.addEventListener('mousedown', () => tone1.play());
 pad2.addEventListener('mousedown', () => tone2.play());
 pad3.addEventListener('mousedown', () => tone3.play());
