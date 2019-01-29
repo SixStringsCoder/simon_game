3da145ac0346e14025e69f2076fe0aaaeeb811ef
steve hanlon <rshanlon@gmail.com>
2019-01-28 18:43:38 -0800

    Refactored adding 3 new functions for Simone to play a challenge, player to play pads and console log shows if correct or not, plus pad eventListeners have been adjusted to make more DRY

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // Challenge Array
 const challenge = [0, 1, 2, 2, 0, 3, 3, 2];
-
-const touchpads = {
-  pad0: { bgcolor: 'rgba(204, 255, 0, 1)'},
-  pad1: { bgcolor: 'rgba(255, 0, 204, 1)'},
-  pad2: { bgcolor: 'rgba(255, 204, 51, 1)'},
-  pad3: { bgcolor: 'rgba(170, 240, 209, 1)'}
-}
+const playerAnswer = [];
 
 // Instantiate audio
 const tone0 = new Audio("audio/tone0.mp3");
@@ -22,25 +16,47 @@ const pad3 = document.querySelector('#pad3');
 const playgame = document.querySelector('#play-game');
 
 // Listeners and Handlers
-pad0.addEventListener('mousedown', () => tone0.play());
-pad1.addEventListener('mousedown', () => tone1.play());
-pad2.addEventListener('mousedown', () => tone2.play());
-pad3.addEventListener('mousedown', () => tone3.play());
-playgame.addEventListener('click', () => playPad(challenge))
+pad0.addEventListener('mousedown', () => youPlayPad(event));
+pad1.addEventListener('mousedown', () => youPlayPad(event));
+pad2.addEventListener('mousedown', () => youPlayPad(event));
+pad3.addEventListener('mousedown', () => youPlayPad(event));
+playgame.addEventListener('click', () => simonesTurn(challenge))
 
 
-
-function playPad(challengeArray) {
+const simonesTurn = (challengeArray) => {
     let index = 0;
     const playChallenge = setInterval(playTone, 500);
     function playTone() {
-      if (index === challengeArray.length-1) {
+      if (index > challengeArray.length-1) {
         clearInterval(playChallenge);
       } else {
         let tone = new Audio(`audio/tone${challengeArray[index]}.mp3`);
         tone.play();
-        document.querySelector(`#pad${challengeArray[index]}`).style.backgroundColor = touchpads[`pad${challengeArray[index]}`].bgcolor;
+        let number = challengeArray[index];
+        $(`#pad${number}`).addClass('opacityFull');
+        setTimeout(() => $(`#pad${number}`).removeClass('opacityFull'), 300);
         index += 1;
       }
     }
+}
+
+const youPlayPad = (event) => {
+  let number = parseInt(event.target.attributes.name.value);
+  const tone = new Audio(`audio/tone${number}.mp3`);
+  tone.play();
+  $(event.target.id).addClass('opacityFull');
+  setTimeout(() => $('event.target.id').removeClass('opacityFull'), 200);
+  playerAnswer.push(number);
+  checkPlayerAnswer(playerAnswer);
+}
+
+
+const checkPlayerAnswer = (answers) => {
+  answers.forEach((answer, index) => {
+    if (answer !== challenge[index]) {
+      console.log("WRONG!")
+    } else {
+      console.log("WINNER");
+    }
+  })
 }
