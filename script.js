e327212fe56bc322cf14c70d80410ce5133c0756
steve hanlon <rshanlon@gmail.com>
2019-01-29 23:14:56 -0800

    Game is working logically now with Simone automatically adding a new tone to the end of her array after challenger successfully plays the right sequence; add randomNumber function to add to end of her array; expanded the checkPlayerAnswer function to call simonesTurn functin; added gameGoing boolean and simonePLaying boolean to help monitor play and game duration

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Challenge Array
-const challenge = [0, 1, 2, 2, 0, 3, 3, 2];
+const challenge = [];
 const playerAnswer = [];
+let simonePlaying = false;
+let gameGoing = false;
 
 // Instantiate audio
 const tone0 = new Audio("audio/tone0.mp3");
@@ -20,15 +22,28 @@ pad0.addEventListener('mousedown', () => youPlayPad(event));
 pad1.addEventListener('mousedown', () => youPlayPad(event));
 pad2.addEventListener('mousedown', () => youPlayPad(event));
 pad3.addEventListener('mousedown', () => youPlayPad(event));
-playgame.addEventListener('click', () => simonesTurn(challenge))
-
+playgame.addEventListener('click', () => {
+    if(!simonePlaying) {
+      gameGoing = true;
+      simonePlaying = true;
+      simonesTurn(challenge);
+      console.log(challenge);
+      playerAnswer.splice(0, playerAnswer.length);
+    }
+    if(!gameGoing) {
+      challenge.splice(0, playerAnswer.length);
+      gameGoing = true;
+    }
+})
 
 const simonesTurn = (challengeArray) => {
+    randomNumber();
     let index = 0;
     const playChallenge = setInterval(playTone, 500);
     function playTone() {
       if (index > challengeArray.length-1) {
         clearInterval(playChallenge);
+        simonePlaying = false;
       } else {
         let tone = new Audio(`audio/tone${challengeArray[index]}.mp3`);
         tone.play();
@@ -42,21 +57,47 @@ const simonesTurn = (challengeArray) => {
 
 const youPlayPad = (event) => {
   let number = parseInt(event.target.attributes.name.value);
+  // play tone
   const tone = new Audio(`audio/tone${number}.mp3`);
   tone.play();
+  // brighten light
   $(`#${event.target.id}`).addClass('opacityFull');
   setTimeout(() => $(`#${event.target.id}`).removeClass('opacityFull'), 200);
+
   playerAnswer.push(number);
   checkPlayerAnswer(playerAnswer);
+  simonePlaying = false;
 }
 
 
 const checkPlayerAnswer = (answers) => {
   answers.forEach((answer, index) => {
     if (answer !== challenge[index]) {
-      console.log("WRONG!")
-    } else {
-      console.log("WINNER");
+      console.log("WRONG! GAME OVER--->", "my: " + playerAnswer, "Simone: " + challenge)
+      playerAnswer.splice(0, playerAnswer.length);
+      challenge.splice(0, challenge.length);
+      gameGoing = false;
+    } else if (answer === challenge[index]) {
+      console.log("CORRECT!");
+      console.log("my: " + playerAnswer, "Simone: " + challenge);
+    }
+    if (answer === challenge[index] && playerAnswer.length === challenge.length) {
+      console.log("MOVING ON");
+      playerAnswer.splice(0, playerAnswer.length);
+      setTimeout(continuePlay, 750);
     }
   })
+}
+
+
+
+const continuePlay = () => {
+  if (gameGoing) {
+    simonesTurn(challenge);
+  }
+}
+
+const randomNumber = () => {
+  let number = Math.floor(Math.random() * 4);
+  challenge.push(number);
 }
